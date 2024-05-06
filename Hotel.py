df5c304ff701623527f8143cbf203253a16f4bb4
Hadi Faraji <H.Faraji@Abnoosgasht.org>
2024-05-06 15:12:50 +0330

    test

--- Hotel.py
+++ Hotel.py
@@ -2,6 +2,7 @@ from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
 from appium.options.android import UiAutomator2Options
 from time import sleep
+import random
 
 desired_caps = {
     "appium:options" :
@@ -16,16 +17,16 @@ desired_caps = {
 appium_server = "http://127.0.0.1:4723"
 appium_options = UiAutomator2Options().load_capabilities(desired_caps)
 driver = webdriver.Remote(appium_server, options=appium_options)
+driver.implicitly_wait(10)
 driver.find_element(by=AppiumBy.XPATH,value='//*[@text="Allow"]').click()
 driver.find_element(by=AppiumBy.XPATH,value='//android.widget.TextView[@text="هتل"]').click()
 sleep(5)
 driver.find_elements(by=AppiumBy.CLASS_NAME,value="android.widget.Button")[0].click()
 sleep(1)
-el1 = driver.find_elements(by=AppiumBy.CLASS_NAME,value="android.widget.EditText")[0]
-el1.send_keys('تهران')
+el1 = driver.find_elements(by=AppiumBy.XPATH, value='//*[@class = "android.view.View"]')
+choice_by_random = random.choice(el1)
+choice_by_random.click()
 sleep(5)
-driver.tap([(906,449)])
-sleep(10)
 driver.find_element(by=AppiumBy.XPATH,value='//*[@text="جستجو"]').click()
 sleep(20)
 
