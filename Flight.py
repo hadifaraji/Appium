fee214f807515fb9c76c145472af6e819ca4f30c
Hadi Faraji <H.Faraji@Abnoosgasht.org>
2024-05-07 12:34:29 +0330

    test

--- Flight.py
+++ Flight.py
@@ -2,6 +2,7 @@ from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
 from appium.options.android import UiAutomator2Options
 from time import sleep
+import random
 
 desired_caps = {
     "appium:options" :
@@ -16,13 +17,14 @@ desired_caps = {
 appium_server = 'http://127.0.0.1:4723'
 appium_options = UiAutomator2Options().load_capabilities(desired_caps)
 driver = webdriver.Remote(appium_server, options=appium_options)
-sleep(1)
+driver.implicitly_wait(15)
 driver.find_element(by=AppiumBy.XPATH,value='//*[@text="Allow"]').click()
-sleep(1)
+sleep(5)
 driver.find_elements(by=AppiumBy.CLASS_NAME, value='android.view.ViewGroup')[5].click()
 sleep(5)
-driver.find_elements(by=AppiumBy.CLASS_NAME, value='android.widget.Button')[2].click()
+driver.find_elements(by=AppiumBy.CLASS_NAME, value='android.widget.Button')[2].click() # مبدا
 sleep(5)
-el1 = driver.find_elements(by=AppiumBy.CLASS_NAME,value="android.widget.EditText")[0]
-el1.send_keys('تهران')
+el1 = driver.find_elements(by=AppiumBy.XPATH,value="//*[@class = 'android.view.View']")
+el2 = random.choice(el1)
+el2.click()
 sleep(5)
