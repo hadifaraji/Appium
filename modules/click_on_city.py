d57aa78f04fc05ebdcf47ad3b03369c7d591fdbd
Hadi Faraji <H.Faraji@Abnoosgasht.org>
2024-05-28 08:11:29 +0330

    test

--- modules/click_on_city.py
+++ modules/click_on_city.py
@@ -6,8 +6,8 @@ class City:
     def __init__(self, driver):
         self.driver = driver
 
-    def click_on_city(self,AppiumBy, sleep):
-        click_on_city = self.driver.find_element(by=AppiumBy.CSS_SELECTOR, value=".android.widget.Button:nth-child(0)")
+    def click_on_city_in_hotel(self,AppiumBy, sleep):
+        click_on_city = self.driver.find_element(by=AppiumBy.CSS_SELECTOR, value='.android.widget.Button:nth-child(0)')
         click_on_city.click()
         sleep(6)
         get_listof_city = self.driver.find_elements(by=AppiumBy.XPATH, value='//*[@class = "android.view.View"]')
